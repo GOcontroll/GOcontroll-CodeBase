a5945908214e6a7a4a113db6c5690643d903e4d2
DeveloperGOcontroll <145349775+DeveloperGOcontroll@users.noreply.github.com>
2026-04-04 09:18:19 +0200

    LED color swap

--- code/iot/Core/Src/tim.c
+++ code/iot/Core/Src/tim.c
@@ -110,7 +110,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     PC7     ------> TIM3_CH2
     PC8     ------> TIM3_CH3
     */
-    GPIO_InitStruct.Pin = LED_B_Pin|LED_R_Pin|LED_G_Pin;
+    GPIO_InitStruct.Pin = LED_B_Pin|LED_G_Pin|LED_R_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
